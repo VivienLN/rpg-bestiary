3fa3bcad7601d7ac061de40b92b854ceac0deab8
VivienLN <vivienleneez@gmail.com>
2023-03-07 11:52:49 +0100

    reindent app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import { defineAsyncComponent, onMounted, reactive, ref } from 'vue'
-import NavMain from './components/NavMain.vue'
-import Loading from './components/Loading.vue'
-import Bestiary from './components/Bestiary.vue'
+  import { RouterLink, RouterView } from 'vue-router'
+  import { defineAsyncComponent, onMounted, reactive, ref } from 'vue'
+  import NavMain from './components/NavMain.vue'
+  import Loading from './components/Loading.vue'
+  import Bestiary from './components/Bestiary.vue'
 
-// const data = reactive({
-//   creatures: null
-// })
-const creatures = ref(null)
+  const creatures = ref(null)
 
-onMounted(async () => {
-  console.log('App mounted')
-  let response = await fetch('/src/data/creatures.json')
-  let data = await response.json()
-  creatures.value = data
-})
+  onMounted(async () => {
+    let response = await fetch('/src/data/creatures.json')
+    let data = await response.json()
+    creatures.value = data
+  })
 </script>
 
 <template>
